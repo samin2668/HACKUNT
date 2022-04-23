d335be1b5778034132f82a4528191f99770be4c3
samin2668 <samin2668@gmail.com>
2022-04-23 18:33:58 -0500

    test files

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import Model
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import Model
 from flask import Flask, request
 import cv2
 import json
@@ -11,11 +11,16 @@ from tensorflow.python.keras.backend import set_session
 import base64
 from datetime import datetime
 
+graph = tf.compat.v1.get_default_graph()
 
-graph = tf.get_default_graph()
 app = Flask(__name__)
 CORS(app)
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 set_session(sess)
 
 model = tf.keras.models.load_model('facenet_keras.h5')
+
+
+def encode_img(path, model):
+    
+
